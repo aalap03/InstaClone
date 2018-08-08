3b1496e29c472d2194206bd06bbfb2481ad97f93
aalap <apatel@e-cobalt.com>
2018-08-08 17:27:47 -0400

    input layout animation fixed.

--- app/src/main/java/com/example/aalap/instaclone/account/AccountEditActivity.kt
+++ app/src/main/java/com/example/aalap/instaclone/account/AccountEditActivity.kt
@@ -1,7 +1,10 @@
 package com.example.aalap.instaclone.account
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.support.design.widget.TextInputLayout
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.View
@@ -11,9 +14,10 @@ import com.example.aalap.instaclone.Constants
 import com.example.aalap.instaclone.R
 import kotlinx.android.synthetic.main.account_edit_profile.*
 import kotlinx.android.synthetic.main.layout_account.*
+import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 
-class AccountEditActivity : AppCompatActivity() {
+class AccountEditActivity : AppCompatActivity(), AnkoLogger {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,17 +27,36 @@ class AccountEditActivity : AppCompatActivity() {
     }
 
     private fun edittextHints() {
-        account_edit_bio.findViewById<EditText>(R.id.input_edit_text).hint = "Bio"
-        account_edit_name.findViewById<EditText>(R.id.input_edit_text).hint = "Name"
-        account_edit_username.findViewById<EditText>(R.id.input_edit_text).hint = "Username"
-        account_edit_email.findViewById<EditText>(R.id.input_edit_text).hint = "Email"
-        account_edit_gender.findViewById<EditText>(R.id.input_edit_text).hint = "Gender"
-        account_edit_phone.findViewById<EditText>(R.id.input_edit_text).hint = "Phone"
-        account_edit_website.findViewById<EditText>(R.id.input_edit_text).hint = "Website"
+        account_edit_bio.findViewById<TextInputLayout>(R.id.input_edit_text_layout).hint = "Bio"
+        account_edit_name.findViewById<TextInputLayout>(R.id.input_edit_text_layout).hint = "Name"
+        account_edit_username.findViewById<TextInputLayout>(R.id.input_edit_text_layout).hint = "Username"
+        account_edit_email.findViewById<TextInputLayout>(R.id.input_edit_text_layout).hint = "Email"
+        account_edit_gender.findViewById<TextInputLayout>(R.id.input_edit_text_layout).hint = "Gender"
+        account_edit_phone.findViewById<TextInputLayout>(R.id.input_edit_text_layout).hint = "Phone"
+        account_edit_website.findViewById<TextInputLayout>(R.id.input_edit_text_layout).hint = "Website"
     }
 
     private fun pickImage() {
+        if( ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE), 5)
+        }
+    }
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode == 5) {
+
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //permission granted yooohoo...
+
+
+
+            } else {
+                ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[0])
+            }
+        }
     }
 
 }
